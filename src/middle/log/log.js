fe7071112ab8320252d4249838926c26b83e4064
Git-Admin <r.t.smith.1984@gmail.com>
2016-05-03 10:04:42 -0400

    Extracted run.sh into bin/start and bin/stop

--- src/middle/log/log.js
+++ src/middle/log/log.js
@@ -14,13 +14,13 @@ var logger = new winston.Logger({
       maxsize: 5242880,
       maxFiles: 5,
       colorize: false
-    }),
+    })/*,
     new winston.transports.Console({
       level: 'debug',
       handleExceptions: true,
       json: false,
       colorize: true
-    })
+    })*/
   ],
   exitOnError: false
 });
