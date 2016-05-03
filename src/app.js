5f885bcba356f7fdfdec9d8bff68b16a487aed6f
Git-Admin <r.t.smith.1984@gmail.com>
2016-05-02 21:23:32 -0400

    Tweaked my crappy little app to return JSON that gets POSTed; moved the run.sh file; cleaned it up to properly set up iptables, as if that matters

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
 app.post('/post', function(req, res) {
-  res.send('Hey there: Your request body: ' + req.body);
+  res.json(req.body);
 });
 
 app.listen(3000, function() {
